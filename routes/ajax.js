75ad8d1f231470362db3620a137d27af2b90f2a9
iondo <ines.ondo.baka@gmail.com>
2019-02-19 17:17:16 +0100

    updated version

--- routes/ajax.js
+++ routes/ajax.js
@@ -4,6 +4,10 @@ const axios = require('axios');
 
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.render('ajax');
+});
+
 router.get('/:city', (req, res, next) => {
   const { city } = req.params;
   axios.get(`http://api.openweathermap.org/data/2.5/weather?APPID=2d6806ccd5f57c6ddbe46da11e9da5b3&q=${city}&units=metric`)
